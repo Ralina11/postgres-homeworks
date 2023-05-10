a6defa14b87974703a21cfbe69c34bbb5ac12df7
Ralina Latypova <Latypovaralina99@gmail.com>
2023-05-09 21:34:50 -0700

    add home work 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(employee_id serial PRIMARY KEY,
+ first_name CHARACTER VARYING(30),
+ last_name CHARACTER VARYING(30),
+ title 	varchar(100) NOT NULL,
+ birth_date varchar(30),
+ notes text
+);
+
+CREATE TABLE customers
+(customer_id varchar(50) PRIMARY KEY,
+ company_name varchar(100) NOT NULL,
+ contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(order_id int PRIMARY KEY,
+ customer_id varchar(50) REFERENCES customers(customer_id) NOT NULL,
+ employee_id int REFERENCES employees(employee_id) NOT NULL,
+ order_date date,
+ ship_city varchar(50)
+);
+
+SELECT * FROM employees
+SELECT * FROM customers
+SELECT * FROM orders
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,74 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+"""пустые списки для чтения файлов csv"""
+result_employees = []
+result_customer = []
+result_orders = []
+
+"""Пути до файлов csv"""
+way_employees = '/Users/ralina/Desktop/все проекты/postgres-homeworks/homework-1/north_data/employees_data.csv'
+way_custom = '/Users/ralina/Desktop/все проекты/postgres-homeworks/homework-1/north_data/customers_data.csv'
+way_orders = '/Users/ralina/Desktop/все проекты/postgres-homeworks/homework-1/north_data/orders_data.csv' \
+
+"""Чтение файла csv"""
+with open(way_employees, newline='') as File:
+    reader = csv.reader(File)
+    result_employees.extend(reader)
+
+with open(way_custom, newline='') as File:
+    reader = csv.reader(File)
+    result_customer.extend(reader)
+
+with open(way_orders, newline='') as File:
+    reader = csv.reader(File)
+    result_orders.extend(reader)
+
+"""Конект к БД"""
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='Ralina:11'
+)
+
+"""Создаем курсор"""
+id = 1
+cur = conn.cursor()
+for row in result_employees:
+    if row[0] == "first_name":
+        continue
+    row.insert(0, id)
+    cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", row)
+    cur.execute("SELECT * FROM employees")
+    conn.commit()
+    id += 1
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+for row in result_customer:
+    if row[0] == "customer_id":
+        continue
+    cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", row)
+    cur.execute("SELECT * FROM customers")
+    conn.commit()
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+for row in result_orders:
+    if row[0] == "order_id":
+        continue
+    cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", row)
+    cur.execute("SELECT * FROM orders")
+    conn.commit()
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+
+
+
+
